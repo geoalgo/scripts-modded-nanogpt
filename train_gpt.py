462f62e83fa143c40a7529d29c71e707d2d86b9a
David  Salinas <david.salinas.pro@gmail.com>
2025-07-31 10:42:31 +0200

    edit

--- train_gpt.py
+++ train_gpt.py
@@ -1,3 +1,4 @@
+from syne_tune import Reporter
 import json
 from pprint import pprint
 import os
@@ -969,6 +970,7 @@ torch.cuda.synchronize()
 t0 = time.perf_counter()
 # begin training
 train_steps = args.num_iterations
+reporter = Reporter()
 for step in range(train_steps + 1):
     last_step = (step == train_steps)
 
@@ -992,7 +994,9 @@ for step in range(train_steps + 1):
         dist.all_reduce(val_loss, op=dist.ReduceOp.AVG)
         metric = {"val_loss": val_loss.item(), "iteration": step // args.val_loss_every}
 
-        print0(f'[tune-metric]: {json.dumps(metric)}', console=True)
+        # TODO handle print0?
+        reporter.report(val_loss=val_loss.item(), train_time=training_time_ms/max(step, 1))
+
         print0(f"step:{step}/{train_steps} val_loss:{val_loss:.4f} train_time:{training_time_ms:.0f}ms step_avg:{training_time_ms/max(step, 1):.2f}ms", console=True)
 
         model.train()
